20f0e20cec6fae10bb1fba17369066bafc4c4b4c
man <a@b.ru>
2023-07-14 21:03:18 +0300

    выполнили слипание в цикле

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,11 +7,9 @@ import java.sql.SQLException;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) {
+public class App {
+    public static void main(String[] args) {
 
         String address = "jdbc:postgresql://10.10.101.200:5500/biblio";
         String user = "postgres";
@@ -38,23 +36,28 @@ public class App
         return connection;
     }
 
-    public static void getTitlesByYear(Connection cnt)
-    {
+    public static void getTitlesByYear(Connection cnt) {
         String q = "SELECT year, title FROM book ORDER BY year";
         //System.out.println(q);
-        int y =0;
+        int y = 0;
         String t = "";
         try {
             ResultSet rs = cnt.createStatement().executeQuery(q);
-            while(rs.next())
-            {
-                y = rs.getInt(1);
-                t = rs.getString(2);
-                System.out.println("    "+y +"     "+t);
+            while (rs.next()) {
+                if (y != rs.getInt(1)) {
+                    if (y!=0 && t!= "")
+                        System.out.println("    " + y + "     " + t);
+                    y = rs.getInt(1);
+                    t = rs.getString(2);
+                }
+                else {
+                    t +=", "+ rs.getString(2);
+                }
             }
+            System.out.println("    " + y + "     " + t);
 
         } catch (SQLException e) {
-            System.out.println("запрос "+q+" не удалось выполнить");
+            System.out.println("запрос " + q + " не удалось выполнить");
             System.out.println(e.getMessage());
         }
 
