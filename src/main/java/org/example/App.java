ec265a6cbb3b417df962feb13f0934c65e71abec
man <a@b.ru>
2023-07-14 20:03:13 +0300

    Добавили в мейвен драйвер Postgre, скопировали и проверили подклбчение к бд

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,32 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        String address = "jdbc:postgresql://10.10.101.200:5500/biblio";
+        String user = "postgres";
+        String pwd = "123";
+        Connection con = connectToDB(address, user, pwd);
+    }
+
+    public static Connection connectToDB(String ad, String us, String pw) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(ad, us, pw);
+            System.out.println("ура! есть коннект");
+        } catch (SQLException e) {
+            System.out.println("не удалось подключиться к " + ad);
+        }
+
+        return connection;
     }
 }
